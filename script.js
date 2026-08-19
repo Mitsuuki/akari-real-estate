38da882a43dc798e75e092a8f6884a1460982fb3
Mitsuuki <123576459+Mitsuuki@users.noreply.github.com>
2026-08-19 10:32:19 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // --- 100% BULLETPROOF CLOUDFLARE URL ---
-const N8N_WEBHOOK_URL = 'https://problems-llp-third-neo.trycloudflare.com/webhook/agency-router';
+const N8N_WEBHOOK_URL = 'https://problems-llp-third-neo.trycloudflare.com/webhook/real-estate-chat';
 
 let currentImageBase64 = "";
 let isSending = false; 
